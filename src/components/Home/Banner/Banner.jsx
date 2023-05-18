77f38fe329175c73ae28c3886e5e11aeabdaa923
shailesh <shaileshkryadav13@gmail.com>
2023-05-18 22:29:41 +0545

    carousel and hover effect done

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,25 +1,23 @@
 import React from "react";
 
 import "./Banner.scss";
-import BannerImg from "../../../assets/banner-img.png";
 
-const Banner = () => {
+
+const Banner = ({title, description, image,width}) => {
     return (
         <div className="hero-banner">
             <div className="content">
                 <div className="text-content">
-                    <h1>SALES</h1>
+                    <h1>{title}</h1>
                     <p>
-                        Convallis interdum purus adipiscing dis parturient
-                        posuere ac a quam a eleifend montes parturient posuere
-                        curae tempor
+                       {description}
                     </p>
                     <div className="ctas">
                         <div className="banner-cta">Read More</div>
                         <div className="banner-cta v2">Shop Now</div>
                     </div>
                 </div>
-                <img className="banner-img" src={BannerImg} />
+                <img className="banner-img" src={image} width={width}/>
             </div>
         </div>
     );
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,16 @@
 import "./Home.scss";
+import img01 from "../../assets/banner-img.png";
+import img02 from "../../assets/Payment-Method-Transparent.png";
+import img03 from "../../assets/laptops.png";
 
 import Banner from "./Banner/Banner";
 const Home = () => {
     return <div className="home">
-        <Banner />
+        <div className="slides">
+        <Banner title="SCALE" description="Buy product from our store and get a flat discount upto 30%. " image={img01} />
+        <Banner title="Payment" description="We accept different payment method and We Value good customer experience" image={img02} width="400px"/>
+        <Banner title="Laptops" description="We make sure that the product, we sell are in good shape and is accourding to the advertisement" image={img03} />
+        </div>
     </div>;
 };
 
